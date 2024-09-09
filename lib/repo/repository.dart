842831393c7394d7f6f8945286d1f3bc70156d8c
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-09 15:02:01 +0500

    completed repository

---
+++ lib/repo/repository.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:external_path/external_path.dart';
+import 'package:flutter/material.dart';
+import 'package:media_scanner/media_scanner.dart';
+import 'package:pure_pixels_wallpaper_app/model/model.dart';
+import 'package:http/http.dart' as http;
+
+class Repository {
+  final String apiKey =
+      "omTqAbSacUVZ7d6rdC9zKQK9aFdBHQmFO9sPmyTJkJPMN28MFw1izNog";
+  final String baseURL = "https://api.pexels.com/v1/";
+
+  Future<List<Images>> getImagesList({required int? pageNumber}) async {
+    String url = '';
+
+    if (pageNumber == null) {
+      url = "$baseURL?per_page=80";
+    } else {
+      url = "$baseURL?per_page=80&page=$pageNumber";
+    }
+
+    List<Images> imagesList = [];
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {'Authorization': apiKey},
+      );
+
+      if (response.statusCode >= 200 && response.statusCode <= 299) {
+        final jsonData = jsonDecode(response.body);
+
+        for (final json in jsonData["photos"] as Iterable) {
+          final image = Images.fromJson(json);
+          imagesList.add(image);
+        }
+      }
+    } catch (_) {}
+    return imagesList;
+  }
+
+  Future<Images> getImagesById({required int id}) async {
+    final url = "${baseURL}photos/$id";
+
+    Images image = Images.emptyConstructor();
+
+    try {
+      final response = await http.get(Uri.parse(url), headers: {
+        'Authorization': apiKey,
+      });
+
+      if (response.statusCode >= 200 && response.statusCode <= 299) {
+        final jsonData = jsonDecode(response.body);
+
+        image = Images.fromJson(jsonData);
+      }
+    } catch (_) {}
+    return image;
+  }
+
+  Future<List<Images>> getImagesBySearch({required String query}) async {
+    final url = "${baseURL}search?query=$query&per_page=80";
+    List<Images> imagesList = [];
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {'Authorization': apiKey},
+      );
+
+      if (response.statusCode >= 200 && response.statusCode <= 299) {
+        final jsonData = jsonDecode(response.body);
+
+        for (final json in jsonData["photos"] as Iterable) {
+          final image = Images.fromJson(json);
+          imagesList.add(image);
+        }
+      }
+    } catch (_) {}
+    return imagesList;
+  }
+
+  Future<void> downloadImage({
+    required String imageUrl,
+    required int imageId,
+    required BuildContext context,
+  }) async {
+    try {
+      final response = await http.get(Uri.parse(imageUrl));
+
+      if (response.statusCode >= 200 && response.statusCode <= 299) {
+        final bytes = response.bodyBytes;
+        final directory = await ExternalPath.getExternalStoragePublicDirectory(
+            ExternalPath.DIRECTORY_DOWNLOADS);
+
+        final file = File("$directory/$imageId.png");
+        await file.writeAsBytes(bytes);
+
+        MediaScanner.loadMedia(path: file.path);
+
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Colors.green,
+              content: Text("Wallpaper has been saved at: ${file.path}"),
+              duration: const Duration(seconds: 2),
+            ),
+          );
+        }
+      }
+    } catch (_) {}
+  }
+}
