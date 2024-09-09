06d11ad45dc7c3eecdb8bebf1d741ad2d6fe9342
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-09 15:09:33 +0500

    added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pure_pixels_wallpaper_app/custom_scroll_behaviour.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      scrollBehavior: CustomScrollBehaviour(),
       home: Scaffold(
         body: Center(
           child: Text('Usama'),
