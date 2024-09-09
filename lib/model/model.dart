13a29d8a73f9880d80335036925f94be3acb850c
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-09 11:36:18 +0500

    added repository class

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,3 +1,23 @@
-class Images{
-  final int imageI
+class Images {
+  final int imageID;
+  final String imageAlt;
+  final String imagePortraitPath;
+
+  const Images({
+    required this.imageID,
+    required this.imageAlt,
+    required this.imagePortraitPath,
+  });
+
+  factory Images.fromJson(Map<String, dynamic> json) => Images(
+    imageID: json["id"] as int,
+    imageAlt: json["alt"] as String,
+    imagePortraitPath: json["src"]["portrait"] as String,
+  );
+
+  Images.emptyConstructor({
+    this.imageID = 0,
+    this.imageAlt = '',
+    this.imagePortraitPath = '',
+});
 }
